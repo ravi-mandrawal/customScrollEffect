3c145a1e399ab868ee66efef08e4d6a67cd86a2e
ravi mandrawal <78255248+ravi-mandrawal@users.noreply.github.com>
2026-01-18 14:05:34 +0000

    delete row files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import NewComp from "./componetA";
-import useCustome from "./custom";
 import "./styles.css";
 import ScrollFunction from "./scrollEffect";
 
 export default function App() {
-  const { count, plusCount, MinCount } = useCustome();
-
   return (
     <div>
-      {/* <NewComp /> */}
       <ScrollFunction />
-      {/* <div className="App">
-        {count}
-        <button
-          onClick={() => {
-            plusCount();
-          }}
-        >
-          Plus
-        </button>
-        <button
-          onClick={() => {
-            MinCount();
-          }}
-        >
-          Min
-        </button>
-      </div> */}
     </div>
   );
 }
